229776b93f664d808893c57b4aee48696c7ed0e0
Karam Guliyev <karamguliyev10@gmail.com>
2024-03-05 21:32:35 +0300

    Auth Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return <h1 className="text-red-800">Shall we begin?</h1>;
+export default async function Home() {
+  return <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100"></div>;
 }
