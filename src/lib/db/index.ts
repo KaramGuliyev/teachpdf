3a3dd61857821ea8eae034083545f1d7f69470f9
Karam Guliyev <karamguliyev10@gmail.com>
2024-03-05 23:27:05 +0300

    Checkpoint

---
+++ src/lib/db/index.ts
@@ -0,0 +1,12 @@
+import { neon, neonConfig, NeonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+neonConfig.fetchConnectionCache = true;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DB Url not found.");
+}
+
+const sql = neon(process.env.DATABASE_URL);
+
+export const db = drizzle(sql);
---
+++ src/lib/db/schema.ts
@@ -0,0 +1,22 @@
+import { PgTable, integer, pgEnum, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const userSystemEnum = pgEnum("user_system_enum", ["system", "user"]);
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdf_name").notNull(),
+  pdfURL: text("pdf_url").notNull(),
+  createdAt: timestamp("createdAt").notNull().defaultNow(),
+  userId: varchar("userId", { length: 256 }).notNull(),
+  fileKey: text("file_key").notNull(),
+});
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("pdf_name")
+    .references(() => chats.id)
+    .notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("createdAt").notNull().defaultNow(),
+  role: userSystemEnum("role").notNull().default("user"),
+});
