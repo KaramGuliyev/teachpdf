e45cc2c4ab214568ce40995643cdb44e6b16b320
Karam Guliyev <karamguliyev10@gmail.com>
2024-03-24 14:55:20 +0400

    File upload to DB

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Providers from "@/components/Providers";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,8 @@ export default function RootLayout({
     <ClerkProvider>
       <Providers>
         <html lang="en">
-          
           <body className={inter.className}>{children}</body>
+          <Toaster />
         </html>
       </Providers>
     </ClerkProvider>
--- src/components/fileUpload.tsx
+++ src/components/fileUpload.tsx
@@ -2,39 +2,58 @@
 
 import { uploadToS3 } from "@/lib/s3";
 import { useMutation } from "@tanstack/react-query";
-import { InboxIcon } from "lucide-react";
+import { InboxIcon, Loader2 } from "lucide-react";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 type Props = {};
 
 const FileUpload = (props: Props) => {
-  const { mutate } = useMutation({
+  const [uploading, setUploading] = useState(false);
+  const { mutate, isPending } = useMutation({
     mutationFn: async ({ file_key, file_name }: { file_key: string; file_name: string }) => {
-      const response = await axios.post("/api/create-chat", { file_key, file_name });
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
       return response.data;
     },
   });
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
     onDrop: async (acceptedFiles) => {
-      console.log(acceptedFiles);
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
-        alert("File size is more than 10MB");
+        // bigger than 10mb!
+        toast.error("File too large");
         return;
       }
 
       try {
+        setUploading(true);
         const data = await uploadToS3(file);
+        console.log("meow", data);
         if (!data?.file_key || !data.file_name) {
-          alert("Something went wrong!");
+          toast.error("Something went wrong");
           return;
         }
-        console.log(data);
+        mutate(data, {
+          onSuccess: ({ chat_id }) => {
+            toast.success("Chat created!");
+          },
+          onError: (err) => {
+            toast.error("Error creating chat");
+            console.error(err);
+          },
+        });
       } catch (error) {
         console.log(error);
+      } finally {
+        setUploading(false);
       }
     },
   });
@@ -47,8 +66,17 @@ const FileUpload = (props: Props) => {
         })}
       >
         <input type="text" {...getInputProps()} />
-        <InboxIcon className="w-10 h-10 text-blue-500" />
-        <p className="mt-2 text-sm text-slate-400">Drag&Drop your PDFs here!</p>
+        {uploading || isPending ? (
+          <>
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">Learning PDF</p>
+          </>
+        ) : (
+          <>
+            <InboxIcon className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drag&Drop your PDFs here!</p>
+          </>
+        )}
       </div>
     </div>
   );
