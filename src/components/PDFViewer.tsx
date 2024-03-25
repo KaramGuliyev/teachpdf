bd0384362c4f5f58f1e20e04937ceb09d3f200e6
Karam Guliyev <karamguliyev10@gmail.com>
2024-03-25 12:11:27 +0400

    Chat Component Start

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -3,8 +3,6 @@ import React from "react";
 type Props = { pdf_url: string };
 
 const PDFViewer = ({ pdf_url }: Props) => {
-    console.log(`https://docs.google.com/gview?url=${pdf_url}&embedded=true`);
-    
   return <iframe src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`} className="w-full h-full"></iframe>;
 };
 
