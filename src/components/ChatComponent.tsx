f7e523033257e8c3ca5b30952971c1f28eb4cbf3
Karam Guliyev <karamguliyev10@gmail.com>
2024-03-25 15:23:06 +0400

    Chat Component Finished

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -12,22 +12,43 @@ import { Message } from "ai";
 type Props = { chatId: number };
 
 const ChatComponent = ({ chatId }: Props) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: async () => {
+      const response = await axios.post<Message[]>("/api/get-messages", {
+        chatId,
+      });
+      return response.data;
+    },
+  });
 
   const { input, handleInputChange, handleSubmit, messages } = useChat({
     api: "/api/chat",
+    body: {
+      chatId,
+    },
+    initialMessages: data || [],
   });
 
+  React.useEffect(() => {
+    const messageContainer = document.getElementById("message-container");
+    if (messageContainer) {
+      messageContainer.scrollTo({
+        top: messageContainer.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
   return (
-    <div className="relative max-h-screen overflow-scroll" id="message-container">
-      {/* header */}
+    <div className="relative h-full w-full max-h-screen overflow-scroll" id="message-container">
       <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
         <h3 className="text-xl font-bold">Chat</h3>
       </div>
 
-      {/* message list */}
-      <MessageList messages={messages} isLoading={false} />
+      <MessageList messages={messages} isLoading={isLoading} />
 
-      <form onSubmit={handleSubmit} className="sticky bottom-0 inset-x-0 px-2 py-4 bg-white">
+      <form onSubmit={handleSubmit} className="sticky absolute bottom-0 left-0 inset-x-0 px-2 py-4 mt-1 bg-white">
         <div className="flex">
           <Input value={input} onChange={handleInputChange} placeholder="Ask any question..." className="w-full" />
           <Button className="bg-blue-600 ml-2">
